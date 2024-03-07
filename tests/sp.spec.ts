3a075731c56cdcf12521fd7ff42153d9ce871878
Tomé Vardasca <tome@vardas.ca>
2024-03-07 23:35:35 +0000

    improve user agent

--- tests/sp.spec.ts
+++ tests/sp.spec.ts
@@ -1,5 +1,17 @@
 import { test } from "@playwright/test";
 
+
+const userAgentStrings = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+];
+
+test.use({
+  userAgent: userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)]
+})
+
 test("test", async ({ page }) => {
   await page.goto("https://photo-contest.sophielagirafe.fr/galerie");
   await page
