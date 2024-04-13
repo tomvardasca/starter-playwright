60c7c88c08633a5fc42f6f2d15a1bfade3218b0a
Tomé Vardasca <tome@vardas.ca>
2024-04-13 21:23:48 +0100

    Update sp.spec.ts

--- tests/sp.spec.ts
+++ tests/sp.spec.ts
@@ -15,10 +15,10 @@ test.use({
 test("test", async ({ page }) => {
   await page.goto("https://photo-contest.sophielagirafe.fr/galerie");
   await page
-    .locator('img[src="photos/mars/500/65e4e4f73a24e3.38168596.jpg"]')
+    .locator('img[src="photos/avril/500/65e4e4f73a24e3.38168596.jpg"]')
     .click();
   const child = page.locator(
-    'img[src="photos/mars/65e4e4f73a24e3.38168596.jpg"]'
+    'img[src="photos/avril/65e4e4f73a24e3.38168596.jpg"]'
   );
   await page
     .locator(".uk-modal-container")
